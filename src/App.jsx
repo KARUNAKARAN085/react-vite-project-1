daebab8b93dcd7a228df8d920de9c10f5eed55e8
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-05-22 13:04:20 +0530

    created multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-// component
-const App = () => {
-  return <h1>Hello, world!</h1>
+// component: Hello
+const Hello = () => {
+  return <h1>Hello, world!-Hello</h1>
 }
+
+
+// component: App
+const App = () => {
+  return (
+    <div>
+      <h1>INDEX-App</h1>
+      <Hello />    
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
 
 export default App;
