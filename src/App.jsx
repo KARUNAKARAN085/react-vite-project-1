1702f782cec8dd115dfe73653e8ebfa50143cde3
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-05-23 11:31:43 +0530

    created array and keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import Hello from "./Components/Hello";
-
-
-// component: App
 const App = () => {
 
-const name = 'KARAN!'
+  const friends =[
+    { id:1,name:"john",age:10},
+    { id:2,name:"kaaa",age:20}
+  ]
 
   return (
     <div>
-      <h1>App Component</h1>
-      <Hello 
-        name={name}  
-      />    
+      <h1>Friends</h1>
+      <ul>
+        {
+        friends.map((friend) =>
+          <li key={friend.id}>{friend.name} {friend.age}</li>
+        )
+      }
+      </ul>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/Components/Hello.jsx
+++ src/Components/Hello.jsx
@@ -1,10 +1,10 @@
 // component: Hello
-const Hello = (props) => {
-    return (
-        <div>
-            <p>Hello {props.name} - Hello Component</p>
-        </div>
-    )
-  }
+// const Hello = (props) => {
+//     return (
+//         <div>
+//             <p>Hello {props.name} - Hello Component</p>
+//         </div>
+//     )
+//   }
 
-  export default Hello;
+//   export default Hello;
