24b5371d397ead4b7cdfb666a19238ab87dee697
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-05-27 12:04:51 +0530

    day5 hooks for increment,decrement and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
-// component
+import { useState } from 'react'
+
 const App = () => {
-  return <h1>Hello, world!</h1>
+  
+  let [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count+1)
+  }
+
+  const handleDecrement = () => {
+    setCount(count-1)
+  }
+
+  const handleReset = () => {
+    setCount(0)
+  }
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button><br /><br />
+      <button onClick={handleDecrement}>Decrement</button><br /><br />
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
