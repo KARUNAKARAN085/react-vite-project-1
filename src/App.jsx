6ec8e9e5ce5d7b4b7f3cc74950b03e7fa7dc666d
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-05-28 12:49:53 +0530

    day6 router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
-// component
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello World!</h1>
+  },
+  {
+    path: "/about",
+    element: <h1>About Us</h1>
+  },
+  {
+    path: "/services",
+    element: <h1>Our Service</h1>
+  },
+  {
+    path: "/contact",
+    element: <h1>Contact Us</h1>
+  },
+  {
+    path: "/products",
+    element: <h1>Our Products</h1>
+  }
+])
+
 const App = () => {
-  return <h1>Hello, world!</h1>
+
+  return <RouterProvider router={router}/>
 }
 
 export default App;
