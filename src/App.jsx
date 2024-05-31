7b9ae36c2e297cfc3ef32606db015ab02c8654d7
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-05-31 11:17:33 +0530

    single component context

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
-// component
+import { useContext } from "react";
+import { useState } from "react";
+import { createContext } from "react";
+
+
+const Namecontext = createContext();
+
+const D = () => {
+  const { name } = useContext(Namecontext)
+
+  return <h1>Hello {name}</h1>
+}
+
+const C = () => {
+
+}
+
+const B = () => {
+
+}
+
 const App = () => {
-  return <h1>Hello, world!</h1>
+
+  const [name, setName] = useState()
+
+  return (
+    <Namecontext.Provider value={{name}}>
+        <B/>
+    </Namecontext.Provider>
+  )
 }
 
 export default App;
