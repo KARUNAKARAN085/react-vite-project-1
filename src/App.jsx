368486438f6c0dcc1fa057c970cd58a922266021
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-03 11:11:01 +0530

    useref - focus on input by button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-// component
+import { useRef } from "react";
+
 const App = () => {
-  return <h1>Hello, world!</h1>
-}
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    inputRef.current.focus()
+  };
+
+  return (
+    <>
+      <input type="text" ref={inputRef} placeholder="input 1"/><br /><br />
+      <input type="text" placeholder="input 2"/><br /><br />
+      <button onClick={handleClick}>Focus input 1</button>
+    </>
+  );
+};
 
 export default App;
