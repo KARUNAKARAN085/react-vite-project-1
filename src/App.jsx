9ab9cdbf1343c4b9888c5fce9e1dde1a4aa91fe6
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-03 11:26:02 +0530

    change value button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { useRef } from "react";
 
 const App = () => {
+  const [state,setState] = useState(0)
   const inputRef = useRef(null);
 
   const handleClick = () => {
     inputRef.current.focus()
   };
+  const handleChange = () => {
+    inputRef.current.value="hello world";
+    setState(state+1);
+  };
+
   console.log("rerendering")
   return (
     <>
       <input type="text" ref={inputRef} placeholder="input 1"/><br /><br />
       <input type="text" placeholder="input 2"/><br /><br />
       <button onClick={handleClick}>Focus input 1</button>
+      <button onClick={handleChange}>Change Value</button>
     </>
   );
 };
