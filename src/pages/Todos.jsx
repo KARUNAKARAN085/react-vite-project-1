00d2c5a16da62745e29d68d678631cd9de261e53
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-05 12:20:54 +0530

    pretier

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -2,32 +2,35 @@ import axios from "axios";
 import { useLoaderData } from "react-router-dom";
 
 //define a loader function
-export const loader = async() => {
-    //make an asyn call fetch data from API
-    const todos = await axios.get("https://665eb2bf1e9017dc16f0f58f.mockapi.io/todos");
-    //return the data
-    return todos.data;
-}
+export const loader = async () => {
+  //make an asyn call fetch data from API
+  const todos = await axios.get(
+    "https://665eb2bf1e9017dc16f0f58f.mockapi.io/todos"
+  );
+  //return the data
+  return todos.data;
+};
 
 const Todos = () => {
-    // use the data using the useloader hook
-    const todos = useLoaderData();
+  // use the data using the useloader hook
+  const todos = useLoaderData();
 
-    console.log(todos)
-  
-    return (
+  console.log(todos);
+
+  return (
     <>
-        <h1>Todos list</h1>
-        <ul>
-            {
-                todos.map((todo) => (
-                    <li key={todo.id}><strong>{todo.title}</strong><br />
-                    <em>{todo.description}</em></li>
-                ))
-            }
-        </ul>
+      <h1>Todos list</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <strong>{todo.title}</strong>
+            <br />
+            <em>{todo.description}</em>
+          </li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default Todos
+export default Todos;
