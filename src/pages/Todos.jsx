b8260a42121ae8583287fd36db16c1cd80b28978
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-05 12:18:02 +0530

    fetch using axios

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,16 +1,12 @@
+import axios from "axios";
 import { useLoaderData } from "react-router-dom";
 
 //define a loader function
 export const loader = async() => {
     //make an asyn call fetch data from API
-    const response = await fetch('https://665eb2bf1e9017dc16f0f58f.mockapi.io/todos',
-    {
-        method: 'GET',
-    });
-    //parse the response
-    const todos = await response.json();
+    const todos = await axios.get("https://665eb2bf1e9017dc16f0f58f.mockapi.io/todos");
     //return the data
-    return todos;
+    return todos.data;
 }
 
 const Todos = () => {
@@ -20,7 +16,17 @@ const Todos = () => {
     console.log(todos)
   
     return (
-    <div>Todos</div>
+    <>
+        <h1>Todos list</h1>
+        <ul>
+            {
+                todos.map((todo) => (
+                    <li key={todo.id}><strong>{todo.title}</strong><br />
+                    <em>{todo.description}</em></li>
+                ))
+            }
+        </ul>
+    </>
   )
 }
 
