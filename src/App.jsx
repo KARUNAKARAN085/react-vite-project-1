99c42d1173bbf61e497914ea82be623599f24368
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-07 14:23:30 +0530

    formik completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, selectCount } from "./features/counter/CounterSlice";
+
 // component
 const App = () => {
-  return <h1>Hello, world!</h1>
+
+  const dispatch = useDispatch();
+  const count = useSelector(selectCount);
+
+  return (
+    <div>
+      <h1>Counter: { count }</h1>
+      <button className="btn btn-primary" onClick={()=>dispatch(increment())}>Increment</button><br /><br />
+      <button className="btn btn-primary" onClick={()=>dispatch(decrement())}>Decrement</button>
+    </div>
+  )
 }
 
 export default App;
