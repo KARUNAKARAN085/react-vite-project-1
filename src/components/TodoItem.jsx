dc4a78d8b09d516eddd28220afd1fdfb2549a208
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-07 10:17:21 +0530

    final commit

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,40 +1,62 @@
-import axios from "axios"
+import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const TodoItem = ({ todo, onUpdateTodo }) => {
+const TodoItem = ({ todo, onUpdateTodo, onDeleteTodo }) => {
 
     const handleCheckboxChange = async () => {
-        try{
+        try {
             const updatedTodo = {
                 ...todo,
                 completed: !todo.completed
             };
+
             const response = await axios.put(
                 `https://665eb2bf1e9017dc16f0f58f.mockapi.io/todos/${todo.id}`,
                 updatedTodo
             );
 
             onUpdateTodo && onUpdateTodo(response.data);
-            
-            }catch(error)
-            {
-                console.error(`Failed to update todo: ${error}`);
-            }
+
+        } catch (error) {
+            console.error(`Failed to update todo: ${error}`);
+        }
+    }
+
+    const handleDeleteTodo = async () => {
+        try {
+            const response = await axios.delete(
+                `https://665eb2bf1e9017dc16f0f58f.mockapi.io/todos/${todo.id}`
+            );
+
+            onDeleteTodo && onDeleteTodo(todo.id);
+
+        } catch (error) {
+            console.error(`Failed to delete todo: ${error}`);
+        }
     }
 
   return (
-    <div className="mx-5">
-      <input 
-        type="checkbox" 
-        checked={todo.completed} 
-        onChange={handleCheckboxChange}
-      />
-      <span>
-        <strong>{todo.title}</strong>
-        <br />
-        <em>{todo.description}</em>
-      </span>
+      <div className="mx-3">
+          <input 
+              type="checkbox" 
+              checked={todo.completed}
+              onChange={handleCheckboxChange}
+          />
+          <span>
+              <strong>{todo.title.toUpperCase()}</strong> : <em>{todo.description}</em>
+          </span> &nbsp;&nbsp;
+          <span>
+              <FontAwesomeIcon
+                  icon={faTrash} 
+                  color="red"
+                  fontSize={16}
+                  onClick={handleDeleteTodo}
+                />
+          </span>
+          <br /><br />
     </div>
-  );
-};
+  )
+}
 
 export default TodoItem;
--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -6,9 +6,8 @@ import { useState } from "react";
 //define a loader function
 export const loader = async () => {
   //make an asyn call fetch data from API
-  const todos = await axios.get(
-    "https://665eb2bf1e9017dc16f0f58f.mockapi.io/todos"
-  );
+  const todos = await axios.get("https://665eb2bf1e9017dc16f0f58f.mockapi.io/todos");
+  
   //return the data
   return {todos: todos.data};
 };
@@ -17,14 +16,49 @@ const Todos = () => {
   // use the data using the useloader hook
   const {todos: initialTodos} = useLoaderData();
   const [todos, setTodos] = useState(initialTodos);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [completed, setCompleted] = useState(false);
 
   const handleUpdateTodo = (updatedTodo) => {
     //update the todo in the state
     setTodos(todos.map(todo => (todo.id === updatedTodo.id ? updatedTodo : todo)))
   }
 
+  const handleCtreateTodo = async (e) => {
+    e.preventDefault();
+    
+    //create a new todo
+    const newTodo ={
+      title,
+      description,
+      completed
+    }
+
+  
+    //api call to create new todo
+    const response = await axios.post(`https://665eb2bf1e9017dc16f0f58f.mockapi.io/todos`,newTodo);
+    
+    if(response) {
+      setTitle('');
+      setDescription('');
+      setCompleted(false);
+
+      //update the state with the new todo
+      response.data && setTodos([...todos, response.data])
+
+      alert(`Todo created successfully!`)
+    }
+  }
+
+  const handleDeleteTodo = async (id) => {
+    //api call to delete todo
+    setTodos(
+      todos.filter((todo) => todo.id !== id)
+    )
+  }
   return (
-    <>
+    <div>
       <h1>Todos list</h1>
         {
             todos.map((todo) => (
@@ -32,10 +66,33 @@ const Todos = () => {
                 key={todo.id}
                 todo={todo}
                 onUpdateTodo={handleUpdateTodo}
+                onDeleteTodo={handleDeleteTodo}
               />
             ))
         }
-    </>
+        <form onSubmit={handleCtreateTodo}>
+          <input 
+            type="text" 
+            placeholder="Enter title" 
+            value={title} 
+            onChange={(e) => setTitle(e.target.value)} 
+          />
+          <input 
+            type="text" 
+            placeholder="Enter description"
+            value={description} 
+            onChange={(e) => setDescription(e.target.value)} 
+          />
+          <select
+            value={completed}
+            onChange={(e) => setCompleted(e.target.value)}
+          >
+            <option value={false}>Not Completed</option>
+            <option value={false}>Completed</option>
+          </select>
+          <button type="submit">Add Todo</button>
+        </form>
+    </div>
   );
 };
 
